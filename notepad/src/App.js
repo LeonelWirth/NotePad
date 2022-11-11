3dd1a2157f944c6fb2c240a428cd0f7926c0468d
LeonelWirth <leo_wirth@hotmail.com>
2022-11-11 20:17:49 -0300

    Se logra utilizar el local storage para almacenar las notas y se realizan cambios en el CSS.

--- notepad/src/App.js
+++ notepad/src/App.js
@@ -6,31 +6,13 @@ import Header from "./components/Header";
 
 
 function App() {
-  const [notes,setNotes] = useState([{
-    id: nanoid(),
-    text: "Note text 1",
-    date: "16/10/2022"
-  },
-  {
-    id: nanoid(),
-    text: "Note text 2",
-    date: "17/10/2022"
-  },
-  {
-    id: nanoid(),
-    text: "Note text 3",
-    date: "18/10/2022"
-  },
-  {
-    id: nanoid(),
-    text: "Note text 4",
-    date: "19/10/2022"
-  }]); // dummy notes
+  const [notes,setNotes] = useState([]); // dummy notes
   const [searchText, setSearchText] = useState("")
   const [darkMode, setDarkMode] = useState(false)
 
   useEffect(()=>{
     const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"))
+    console.log(savedNotes)
     if(savedNotes){
       setNotes(savedNotes)
     }
@@ -38,7 +20,7 @@ function App() {
 
   useEffect(()=>{
     localStorage.setItem("react-notes-app-data", JSON.stringify(notes))
-  }, [notes]) // Use the local storage to save the data from the notes, so that when i clos the tab, i dont lose the data.
+  }, [notes]) // Use the local storage to save the data from the notes, so that when i close the tab, i dont lose the data.
 
   const addNote = (text) => {
     const date = new Date()
@@ -50,6 +32,7 @@ function App() {
     }
     const newNotes = [...notes, newNote]
     setNotes(newNotes)
+    
   }
 
   const deleteNote = (id) => {
@@ -58,12 +41,11 @@ function App() {
   }
 
 
-
   return (
     <div className={`${darkMode && `dark-mode`}`}>
       {/* With this div i select or not the darkmode */}
       <div className="container">
-        <Header handleToggleDarkMode={setDarkMode}/>
+        <Header handleToggleDarkMode={setDarkMode} darkMode={darkMode}/>
         <Search handleSearchNote={setSearchText}/>
         <NoteList notes = {notes.filter((note)=> note.text.toLocaleLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
         {/* The notes ar filtered previous to pass them to the note list, so i have all the notes in the original state */}
--- notepad/src/components/Header.jsx
+++ notepad/src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Header({handleToggleDarkMode}) {
+function Header({handleToggleDarkMode, darkMode}) {
     return ( <div className="header">
         <h1>Notes</h1>
-        <button className="button" onClick={() => handleToggleDarkMode((previousDarkMode)=> !previousDarkMode)}>Toggle mode</button>
+        <button className="button" onClick={() => handleToggleDarkMode((previousDarkMode)=> !previousDarkMode)}>{darkMode?"Light mode":"Dark mode"}</button>
     </div> );
 }
 
